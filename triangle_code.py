0fc6a48df0bc9276dab6566f04d2095e21fdf1bf
Sandesh Rakhonde <rakhondesandesh@gmail.com>
2026-03-11 20:18:01 +0530

    NA

---
+++ triangle_code.py
@@ -0,0 +1,7 @@
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+if number1 + number2 > number3 and number2 + number3 > number1 and number3 + number1 > number2:
+    print("Valid Triangle")
+else:
+    print("Invalid Triangle")
